58ef1994269775a8d81e7673fdbff55125cc61a0
Grzegorz Koza <kozagrzegorz@interia.pl>
2018-01-22 16:45:39 +0100

    Add update

--- scripts.js
+++ scripts.js
@@ -12,8 +12,16 @@ console.log(textVelociraptorReplace);
 
 console.log(textVelociraptorReplace.length/2);
 
-var halfTextVelociraptorReplace = textVelociraptorReplace.slice(0, 72);
+var halfTextVelociraptorReplace = textVelociraptorReplace.slice(0, text.length/2);
 
 console.log(halfTextVelociraptorReplace.length);
 
 console.log(halfTextVelociraptorReplace);
+
+var text2 = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+
+console.log(text2.substr(0, text2.length - 90));
+	
+var text3 = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
+console.log(text3.replace(' ', ''));
+ 
